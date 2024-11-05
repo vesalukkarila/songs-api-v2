1fe5d79b4cbfe6781a3f31046ae89898c374f821
Vesa Lukkarila <github@vesalukkarila.com>
2024-11-05 14:36:11 +0200

    initial commit

---
+++ src/main/java/com/vesalukkarila/songs_api_v2/SongsApiV2Application.java
@@ -0,0 +1,13 @@
+package com.vesalukkarila.songs_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongsApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vesalukkarila/songs_api_v2/SongsApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vesalukkarila.songs_api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
